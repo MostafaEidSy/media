81e2b0b4ad36a78163df856e0c64b572df251e89
MostafaEidSy <72385704+MostafaEidSy@users.noreply.github.com>
2021-03-16 16:29:28 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -12,30 +12,20 @@ Route::group(['namespace' => 'Site'], function (){
     });
    Route::get('/', 'GeneralController@index')->name('index');
    Route::get('/pricing', 'GeneralController@pricing')->name('pricing');
-   Route::get('/{slug}', 'GeneralController@showDetails')->name('show.details');
+   Route::get('/show/{slug}', 'GeneralController@showDetails')->name('show.details');
    Route::get('/page/{slug}', 'GeneralController@showPage')->name('show.page');
    Route::get('/category/{slug}', 'GeneralController@showCategory')->name('show.category');
    Route::group(['middleware' => 'auth:web'], function (){
        Route::group(['middleware' => ['role:user']], function (){
-           Route::get('/demo/demo', function (){echo 'ok';});
-       });
-       Route::get('/role/role', function (){
-           if (auth()->user()->can('user')){
-               return redirect()->route('index2');
-           }
-           $user = \App\Models\User::where('id', auth()->user()->id)->first();
-           auth()->user()->assignRole('user');
-           auth()->user()->givePermissionTo('Subscriber');
-           return redirect()->back();
+           Route::get('/add-favorites/{id}', 'GeneralController@addFavorites')->name('add.favorites');
+           Route::post('/add-comment', 'GeneralController@addComment')->name('add.comment');
+           Route::get('/watch-show/{slug}/{videoId?}', 'GeneralController@watchShow')->name('watch.show');
+           Route::get('/watch-video/{slug}', 'GeneralController@watchVideo')->name('watch.video');
        });
        Route::get('/user/logout', 'GeneralController@logout')->name('logout');
        Route::get('/user/manage-profile', 'GeneralController@manage')->name('manage.profile');
        Route::post('/user/manage-profile/{id}', 'GeneralController@updateManage')->name('manage.profile.update');
        Route::get('/user/setting-profile', 'GeneralController@settingProfile')->name('manage.profile.setting');
-       Route::get('/add-favorites/{id}', 'GeneralController@addFavorites')->name('add.favorites');
-       Route::get('/watch-show/{slug}/{videoId?}', 'GeneralController@watchShow')->name('watch.show');
-       Route::get('/watch-video/{slug}', 'GeneralController@watchVideo')->name('watch.video');
-       Route::post('/add-comment', 'GeneralController@addComment')->name('add.comment');
        Route::get('/{id}/checkout/{name?}', 'GeneralController@checkout')->name('checkout');
        Route::get('/subscription/{planId?}/{subscriptionId?}/{billingToken?}/{orderID?}', 'PaymentController@subscription')->name('create-agreement');
    });
